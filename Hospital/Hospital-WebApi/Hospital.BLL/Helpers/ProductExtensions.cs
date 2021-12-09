fc1b483c7017b86a5d59217a7d0aabf0f0aa9fe7
ulvi9 <ulvizm@code.edu.az>
2021-12-09 21:49:03 +0400

    feat:ProductExtensions Filter

--- Hospital/Hospital-WebApi/Hospital.BLL/Helpers/ProductExtensions.cs
+++ Hospital/Hospital-WebApi/Hospital.BLL/Helpers/ProductExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Hospital.DAL.Entities;
 
@@ -22,6 +23,24 @@ namespace Hospital.BLL.Helpers
             if (String.IsNullOrEmpty(searchTerm)) return query;
             string lowerOrTrimSearchTerm = searchTerm.ToLower().Trim();
             return query.Where(p => p.Name.ToLower().Contains(lowerOrTrimSearchTerm));
+        }
+
+        public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types)
+        {
+            var brandList = new List<string>();
+            var typeList = new List<string>();
+            if (!String.IsNullOrEmpty(brands))
+            {
+                brandList.AddRange(brands.ToLower().Split(',').ToList());
+            }
+            if (!String.IsNullOrEmpty(types))
+            {
+                brandList.AddRange(types.ToLower().Split(',').ToList());
+            }
+
+            query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Name.ToLower()));//brand
+            query = query.Where(p => typeList.Count == 0 || typeList.Contains(p.Name.ToLower()));//type
+            return query;
         }
     }
 }
